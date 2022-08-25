2bd93a3fad8077b87d47464ea75bed391850aec9
ashlynhar <112111975+ashlynhar@users.noreply.github.com>
2022-08-25 20:15:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //Replace With Your Own Github Username
-const Github_Username = "OlikaX404";
+const Github_Username = "ashlynhar";
 
 //Replace With Your Own Email ID
-const Email_Id = "olikaamsalu8@gmail.com";
+const Email_Id = "ashlyn_harris.666@icloud.com";
 
 var xhr = new XMLHttpRequest();
 xhr.addEventListener("readystatechange", function() {
